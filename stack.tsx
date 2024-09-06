60d0e5efa95f43789885ab4721df58ff0d45a8da
Abhinandan Gautam <abhigautam7899@gmail.com>
2024-09-06 20:43:36 +0530

    stack.tsx file chnages removed urls

--- stack.tsx
+++ stack.tsx
@@ -1,12 +1,21 @@
+// import "server-only";
+
+// import { StackServerApp } from "@stackframe/stack";
+
+// export const stackServerApp = new StackServerApp({
+//   tokenStore: "nextjs-cookie",
+
+//   urls:{
+//     signIn : '/auth/signIn',
+//     signUp : '/auth/signUp',
+//   },
+//   callback: '/api/v1/auth/oauth/callback/:provider'
+// });
+
 import "server-only";
 
 import { StackServerApp } from "@stackframe/stack";
 
 export const stackServerApp = new StackServerApp({
   tokenStore: "nextjs-cookie",
-
-  urls:{
-    signIn : '/auth/signIn',
-    signUp : '/auth/signUp',
-  }
 });
