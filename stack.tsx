9dd6e8e7dca0d15355a7a346e960a47098660e07
Abhinandan Gautam <abhigautam7899@gmail.com>
2024-09-07 08:08:02 +0530

    stack tsx changes

--- stack.tsx
+++ stack.tsx
@@ -1,6 +1,6 @@
-// import "server-only";
+import "server-only";
 
-// import { StackServerApp } from "@stackframe/stack";
+import { StackServerApp } from "@stackframe/stack";
 
 // export const stackServerApp = new StackServerApp({
 //   tokenStore: "nextjs-cookie",
@@ -11,15 +11,13 @@
 //   },
 //   callback: '/api/v1/auth/oauth/callback/:provider'
 // });
-
-import "server-only";
-
-import { StackServerApp } from "@stackframe/stack";
-
 export const stackServerApp = new StackServerApp({
   tokenStore: "nextjs-cookie",
   urls: {
     signIn: '/auth/signIn',
     signUp: '/auth/signUp',
-  }
+    afterSignIn: '/expense-tracker',
+    afterSignUp: '/expense-tracker',
+  },
+  
 });
